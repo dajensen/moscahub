63085c52b829bb72db70b03458cf2bdda62cb191
Dave Jensen <screamingchameleons1@gmail.com>
2017-02-04 12:13:09 -0700

    Just made it show full on purple.  Because blue and red are good for these plants.  Or so I hear.

--- clients/setpattern.js
+++ clients/setpattern.js
@@ -6,9 +6,7 @@ var client  = mqtt.connect('mqtt://zerohub:1883')
 client.on('connect', function () {
   var newcolor = {
     pattern: [
-      {red: 127, green: 0, blue: 127},
-      {red: 0, green: 0, blue: 127},
-      {red: 127, green: 0, blue: 0}
+      {red: 127, green: 0, blue: 127}
     ]
   };
   client.publish('plid/pattern', JSON.stringify(newcolor))
