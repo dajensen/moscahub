8309646fda37635eb4fbd40c99dd64f2aee1b9c1
Dave Jensen <screamingchameleons1@gmail.com>
2017-02-04 12:03:17 -0700

    Added a file for setting the color of the lid of the predatory plant enclosure.

---
+++ clients/setpattern.js
@@ -0,0 +1,26 @@
+var mqtt = require('mqtt')
+
+var client  = mqtt.connect('mqtt://zerohub:1883')
+ 
+
+client.on('connect', function () {
+  var newcolor = {
+    pattern: [
+      {red: 127, green: 0, blue: 127},
+      {red: 0, green: 0, blue: 127},
+      {red: 127, green: 0, blue: 0}
+    ]
+  };
+  client.publish('plid/pattern', JSON.stringify(newcolor))
+  client.end()
+})
+ 
+
+//      {red: 0, green: 0, blue: 0},
+//      {red: 0, green: 0, blue: 127},
+//      {red: 127, green: 0, blue: 0},
+//      {red: 0, green: 127, blue: 0},
+//      {red: 127, green: 127, blue: 0},
+//      {red: 127, green: 0, blue: 127},
+//      {red: 0, green: 127, blue: 127},
+//      {red: 127, green: 127, blue: 127}
